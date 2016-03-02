707e81892f781c69d9a64f616967a58eb33845f4
James Richard <ketzu@me.com>
2016-03-02 04:36:01 +0000

    Update Package.swift so we can work with Xcode

--- Package.swift
+++ Package.swift
@@ -4,3 +4,11 @@ let package = Package(
     name: "Strand",
     dependencies: []
 )
+
+//with the new swiftpm we have to force it to create a  lib so that we can use it
+//from xcode. this will become unnecessary once official xcode+swiftpm support is done.
+//watch progress: https://github.com/apple/swift-package-manager/compare/xcodeproj?expand=1
+
+let libStrand = Product(name: "Strand", type: .Library(.Dynamic), modules: "Strand")
+products.append(libStrand)
+
