bd738fc139a10318e6fa3b9baba3cd5b7c36ba11
ketzusaka <james@weebly.com>
2016-04-19 07:57:35 -0700

    Use SwiftPM-built xcodeproj

--- Package.swift
+++ Package.swift
@@ -5,12 +5,3 @@ let package = Package(
     dependencies: []
 )
 
-//with the new swiftpm we have to force it to create a  lib so that we can use it
-//from xcode. this will become unnecessary once official xcode+swiftpm support is done.
-//watch progress: https://github.com/apple/swift-package-manager/compare/xcodeproj?expand=1
-
-// Thanks to czechboy0 & the Vapor project for this
-
-let libStrand = Product(name: "Strand", type: .Library(.Dynamic), modules: "Strand")
-products.append(libStrand)
-
